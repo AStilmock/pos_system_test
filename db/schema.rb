93e579be3ddbc341aca74bf868380145b0fcdfc2
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-04-18 11:32:49 -0600

    create discount table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_18_172209) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_18_173210) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "discounts", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.string "description"
+    t.integer "price_percent"
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.integer "phone"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_employees_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -24,4 +45,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_172209) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "employees", "users"
 end

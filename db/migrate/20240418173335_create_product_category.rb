9d8fc4f519b427a48be83b3063e4f6d3726d8dd1
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-04-18 11:33:52 -0600

    create product category table

---
+++ db/migrate/20240418173335_create_product_category.rb
@@ -0,0 +1,10 @@
+class CreateProductCategory < ActiveRecord::Migration[7.0]
+  def change
+    create_table :product_categories do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_18_173210) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_18_173335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_18_173210) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_employees_on_user_id"
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|

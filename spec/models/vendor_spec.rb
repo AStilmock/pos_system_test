fbb30931ce94937a6897ec5b06d4eae0d82d6143
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-04-19 15:52:16 -0600

    begin model relationship and validation testing

--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Vendor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    
+  end
+
+  describe "validations" do
+    
+  end
 end

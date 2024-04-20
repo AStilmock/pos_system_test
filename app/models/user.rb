1048aec454d6e188e6546cf157e0579db772c55d
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-04-19 23:06:55 -0600

    updates for user model testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_many :employees
+  has_many :invoices
+  has_many :purchase_orders
+  has_many :sales
+
+  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true, confirmation: true
+
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "relationships" do
-    
+    it { should have_many :employees }
+    it { should have_many :invoices }
+    it { should have_many :purchase_orders }
+    it { should have_many :sales }
   end
 
   describe "validations" do
-    
+    it { should validate_presence_of(:username)}
+    it { should validate_uniqueness_of(:username)}
+    it { should validate_presence_of(:email)}
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :password }
+    it { should validate_confirmation_of :password }
   end
 end

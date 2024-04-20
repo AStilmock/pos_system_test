30e31727ecdd5477c9aab15bb03f81413bc527ad
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-04-20 07:46:50 -0600

    added vendor relationship and association testing

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,2 +1,12 @@
 class Vendor < ApplicationRecord
+  has_many :purchase_orders
+  has_many :items
+  # has_many :product_categories.through(:items)
+  has_many :product_categories, through: :items
+
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :address
+  validates_presence_of :email
+  validates_presence_of :phone
 end

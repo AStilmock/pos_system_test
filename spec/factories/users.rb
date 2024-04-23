10c2de99fe1dbbde320ce0a4efa19478bf01919e
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-04-23 16:36:36 -0600

    created user model factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    username { Faker::Internet.username }
+    email { Faker::Internet.email }
+    password_digest { "test" }
+    permission { 1 }
+    discount_level { 1 }
   end
 end

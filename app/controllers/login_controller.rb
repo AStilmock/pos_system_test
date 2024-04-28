a4510c2c41e391ba872877a0da7cc2e9b10db1a6
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-04-27 22:15:21 -0600

    controller updates for user/shopping index page testing

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -8,6 +8,14 @@ class LoginController < ApplicationController
   end
 
   def create
+    # user = User.find_by(email: params[:email])
+    # if user && user.authenticate(params[:password])
+    #   set_user_session(user)
+    #   redirect_to root_path, notice: "Logged in successfully"
+    # else
+    #   flash.now[:alert] = "Invalid email or password"
+    #   render :new
+    # end
     redirect_to user_shopping_index_path
   end
 end
--- app/controllers/user/shopping_controller.rb
+++ app/controllers/user/shopping_controller.rb
@@ -1,5 +1,7 @@
 class User::ShoppingController < ApplicationController
   def index
-
+    @user = User.find_by_username(params[:username])
+    @username = @user.username
+    # require 'pry'; binding.pry
   end
 end

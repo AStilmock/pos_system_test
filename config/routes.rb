fc5cb983bdb9e9b542b63696f31240110ca2799e
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-04-27 18:00:28 -0600

    updated routes and completed landing page testing.

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get "/", to: "landing#index"
+  resources :shopping, only: [:index]
+  resources :about, only: [:index]
+  resources :login, only: [:index]
 end
--- spec/features/landing/index_spec.rb
+++ spec/features/landing/index_spec.rb
@@ -2,29 +2,44 @@ require 'rails_helper'
 
 RSpec.describe 'Website Landing Page', type: :feature do
   describe 'index page' do
-    it 'displays welcome banner' do
+    before :each do
       visit "/"
-      expect(page).to have_content("Welcome to the TEST POS SYSYEM WEBSITE - grab all your test merchandise here before it's all gone!")
+    end
+    it 'displays welcome banner' do
+      expect(page).to have_content("Welcome to the TEST POS SYSYEM WEBSITE")
+      expect(page).to have_content("Grab all your test merchandise here before it disappears!!")
     end
 
     it 'displays nav bar' do
-
+      expect(page).to have_link("Home")
+      expect(page).to have_link("Shop")
+      expect(page).to have_link("About")
+      expect(page).to have_link("Login")
     end
 
-    it 'displays footer' do
-
+    it 'clicks link to home' do
+      click_link("Home")
+      expect(page).to have_current_path("/")
     end
 
-    it 'link to shop' do
-      
+    it 'clicks link to about' do
+      click_link("About")
+      expect(page).to have_current_path("/about")
     end
 
-    it 'link to about' do
+    it 'clicks link to shopping' do
+      click_link("Shop")
+      expect(page).to have_current_path("/shopping")
+    end
 
+    it 'clicks link to login' do
+      click_link("Login")
+      expect(page).to have_current_path("/login")
     end
 
     it 'has website info text on page' do
-
+      expect(page).to have_content("This is a test website for a point of sale system.")
+      expect(page).to have_content("All sales are final, and all merchandise is fake.")
     end
   end
 end

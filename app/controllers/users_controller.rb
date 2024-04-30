24d543a0da29f1c4a6fc37f801b60e9bb0e39d1a
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-04-30 16:41:18 -0600

    Adds sorcery modules

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: [:index, :new, :create]
 
-  
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    redirect_to user_shopping_path
+  end
 
   private 
   def user_params
